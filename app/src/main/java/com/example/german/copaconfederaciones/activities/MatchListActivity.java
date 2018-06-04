56b2e3e925e868e4a74684165fa6d83752c8b3ce
germang <german.guevara@xpectra.com>
2018-06-04 14:54:01 -0400

    recyclerview init

---
+++ app/src/main/java/com/example/german/copaconfederaciones/activities/MatchListActivity.java
@@ -0,0 +1,15 @@
+package com.example.german.copaconfederaciones.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.german.copaconfederaciones.R;
+
+public class MatchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_list);
+    }
+}
